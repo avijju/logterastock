7af22ee7d40a50f476c997515b72e66f92912d52
Avinash Bhatt <arthadevlopment@gmail.com>
2021-07-11 22:26:47 +0530

    new function

--- app.py
+++ app.py
@@ -24,6 +24,8 @@ from decimal import Decimal
 from typing import Optional, List
 import requests
 import csv
+import time
+import ast
 from polygon import WebSocketClient, STOCKS_CLUSTER, CRYPTO_CLUSTER, FOREX_CLUSTER
 
 app = Flask(__name__)
@@ -215,7 +217,83 @@ def Get_Stock():
     # )
     return str(aa)
 
+@app.route('/avpersec')
+def Avpersec():
+    key = 'DJwpTfnSeTL0T9Ie3nPHjpwd466R3WlM'
+    messages = []
+   
+    
+    my_client = WebSocketClient(STOCKS_CLUSTER, key, my_custom_process_message(messages))  
+    my_client.run_async() 
+       
+    my_client.subscribe("A.*")  # Stock data
+    time.sleep(1)
 
+    my_client.close_connection()
+
+    df = pd.DataFrame(messages)
+
+    df = df.iloc[5:, 0].to_frame()
+    df.columns = ["data"]
+    df["data"] = df["data"].astype("str")
+
+    df = pd.json_normalize(df["data"].apply(lambda x : dict(eval(x))))
+    if df.empty:
+        print('DataFrame is empty!')
+    else:
+        print(df)
+          
+        connect_string = 'mysql+pymysql://{}:{}@{}/{}?charset=utf8mb4'.format("avi", "logtera", "5.189.178.77",  "Stock")
+    
+        engine = create_engine(connect_string)
+    
+        df.to_sql("aggregates", engine, if_exists='append', index=False)
+    #else:
+    print("Finally finished!")
+
+@app.route('/avpermin')
+def Avpermin():
+    key = 'DJwpTfnSeTL0T9Ie3nPHjpwd466R3WlM'
+    messages = []
+   
+    
+    my_client = WebSocketClient(STOCKS_CLUSTER, key, my_custom_process_message(messages))  
+    my_client.run_async() 
+       
+    my_client.subscribe("AM.*")  # Stock data
+    time.sleep(1)
+
+    my_client.close_connection()
+
+    df = pd.DataFrame(messages)
+
+    df = df.iloc[5:, 0].to_frame()
+    df.columns = ["data"]
+    df["data"] = df["data"].astype("str")
+
+    df = pd.json_normalize(df["data"].apply(lambda x : dict(eval(x))))
+    if df.empty:
+        print('DataFrame is empty!')
+    else:
+        print(df)
+          
+        connect_string = 'mysql+pymysql://{}:{}@{}/{}?charset=utf8mb4'.format("avi", "logtera", "5.189.178.77",  "Stock")
+    
+        engine = create_engine(connect_string)
+    
+        df.to_sql("aggregates", engine, if_exists='append', index=False)
+    #else:
+    print("Finally finished!")
+
+def my_custom_process_message(messages: List[str]):
+    
+    def add_message_to_list(message):
+        
+        messages.append(ast.literal_eval(message))
+
+    return add_message_to_list
+
+    
 class Day:
     c: float
     h: float
--- stock.py
+++ stock.py
@@ -8,55 +8,24 @@ from sqlalchemy import create_engine
 import mysql.connector as msql
 
 def my_custom_process_message(messages: List[str]):
-    """
-        Custom processing function for incoming streaming messages.
-    """
+    
     def add_message_to_list(message):
-        """
-            Simple function that parses dict objects from incoming message.
-        """
+        
         messages.append(ast.literal_eval(message))
 
     return add_message_to_list
 
 
 def main(waiting_time = 1):
-    """
-        Main function which connects to live stream data, and saves incoming data over
-        some pre-determined time in an sqlite database.
-    """
+    
     key = 'DJwpTfnSeTL0T9Ie3nPHjpwd466R3WlM'
     messages = []
    
-    #my_client = WebSocketClient(CRYPTO_CLUSTER, key, my_custom_process_message(messages))
-    #my_client = WebSocketClient(FOREX_CLUSTER, key, my_custom_process_message(messages))
     
-    #db = msql.connect(host='34.136.65.150',user='avi', passwd='logtera',       db='Stock')
-
-# This line is that you need
-    #cursor = db.cursor()
-    #cursor.execute("SELECT count(*) FROM stocksymbols ")
-    #result = cursor.fetchall()
-    # for row in result:
-    #     count=row[0]
-    # loopcount=count/100
-    # for x in range(int(loopcount)):
-    #     offsets=x*100
-    #     print(offsets)
-    #     query = ("SELECT  GROUP_CONCAT(CONCAT('""', Ticker, '""' )) FROM (SELECT Ticker FROM stocksymbols LIMIT 100 offset {} ) AS stock").format(offsets)   
-    #     print(query) 
-    #     cursor.execute(query)
-    #for x in range(6):
-        # result = cursor.fetchall()
-        # for row in result:
-        #     text=row[0]
-        # print(text)
     my_client = WebSocketClient(STOCKS_CLUSTER, key, my_custom_process_message(messages))  
     my_client.run_async() 
-       # my_client.subscribe(text)  # Stock data
+       
     my_client.subscribe("A.*")  # Stock data
-    #my_client.subscribe("XA.BTC-USD", "XA.ETH-USD", "XA.LTC-USD")  # Crypto data
-    #my_client.subscribe("C.USD/CNH", "C.USD/EUR")  # Forex data
     time.sleep(waiting_time)
 
     my_client.close_connection()
@@ -72,16 +41,11 @@ def main(waiting_time = 1):
         print('DataFrame is empty!')
     else:
         print(df)
-          #  df = df[df.ev != 'status']
-            #df.drop(["status"], inplace = True)
-           # df=df[['ev', 'sym','i', 'x','p', 's','t', 'z']]
+          
         connect_string = 'mysql+pymysql://{}:{}@{}/{}?charset=utf8mb4'.format("avi", "logtera", "5.189.178.77",  "Stock")
-    #with sqlite3.connect("realtime_crypto.sqlite") as conn:
-        #df.to_sql("data", con=conn, if_exists="append", index=False)
+    
         engine = create_engine(connect_string)
-    #print(df)
-    # export data to sqlite
-    #with sqlite3.connect("realtime_crypto.sqlite") as conn:
+    
         df.to_sql("aggregates", engine, if_exists='append', index=False)
     #else:
     print("Finally finished!")
